fc3b909bb235ba490d6533890b4388dd162e43e8
Stewart Smith <stewart98@mac.com>
2023-08-20 22:07:36 -0700

    Add files via upload

---
+++ background-quotes-main/app.js
@@ -0,0 +1,46 @@
+const api_url = "https://zenquotes.io/api/quotes/";
+
+const authorTarget = document.querySelector("#authorTag");
+const quoteTarget = document.querySelector("#quoteTag");
+
+async function getapi(url) {
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors",
+    credentials:"include",
+    headers: {
+      "Content-Type":"application/json"
+      // "Access-Control-Allow-Origin":"*"
+    }
+  });
+  console.log(response);
+  var data = await response.json();
+  console.log(data[0]);
+  return data[0];
+}
+
+function writeAuthor(obj) {
+  let fetchAuthor = obj["a"];
+  authorTarget.innerHTML = "AUTHOR: " + fetchAuthor;
+}
+
+function writeQuote(obj) {
+  let fetchQuote = obj["q"];
+  quoteTarget.innerHTML = fetchQuote;
+}
+
+function main() {
+  let data = {}
+  try {
+    data = getapi(api_url);
+  }
+  catch {
+    data = {"q":"error", "a": "error"};
+    console.log("API failed to fetch data")
+  }
+  writeAuthor(data);
+  writeQuote(data);
+}
+
+console.log("test")
+main()
